05e5d1d095de2dab7445e1195865001a40b1f588
Unknown <fleiva@trazalog.com>
2019-06-18 20:39:48 -0300

    Constants de Usuario Admin y Pass

--- application/config/constants.php
+++ application/config/constants.php
@@ -105,6 +105,7 @@ define('CMP_ALM', 'traz-comp-almacen');
 define('BONITA_URL', 'http://localhost:8080/bonita/');  
 define('BPM_ADMIN_USER', 'mantenedor1');
 define('BPM_ADMIN_PASS', 'bpm');
+define('BPM_USER_PASS', 'bpm');
 
 
 /*
