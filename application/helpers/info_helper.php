c9989f56eed32b706dca9b98d5ad0c5c5af11178
Unknown <fleiva@trazalog.com>
2019-06-23 22:58:10 -0300

    Bugs Almacen

--- application/helpers/info_helper.php
+++ application/helpers/info_helper.php
@@ -74,6 +74,10 @@ if (!function_exists('info_orden')) {
 
     function estadoPedido($estado){
         switch ($estado) {
+            case 'Creada':
+              return bolita($estado ,'purple');
+              break;
+            
             case 'Solicitado':
                 return bolita($estado ,'orange');
                 break;
