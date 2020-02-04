75f293501699205a322e19e8bb18e21dfe55d05f
fguardia <fguardia@trazalog.com>
2020-02-04 17:19:46 -0300

    #285- Rearmar la Pantalla de Solicitud de Servicio

--- application/models/Sservicios.php
+++ application/models/Sservicios.php
@@ -149,26 +149,20 @@ class Sservicios extends CI_Model
 			}
 			else
 			{			
-				$equipId  = $data['equip'];//
-				$solicita = $data['solici'];//
-				$dia      = $data['fecha'];//
-				$dia      = explode('-', $dia);
-				$hora     = $data['hora'];//
-				$min      = $data['min'];//
-				$falla    = $data['falla'];//
+				$equipId  = $this->input->post('equipo');//
+				$falla    = $this->input->post('falla');//
 				$userdata = $this->session->userdata('user_data');
 				$empId    = $userdata[0]['id_empresa'];
 
 				$userdata = $this->session->userdata('user_data');
 				$usrId    = $userdata[0]['usrId'];     // guarda usuario logueado
+				$usrName  = $userdata[0]['usrName'];
 				$insert   = array(
 					'f_solicitado' => date('Y-m-d H:i:s'), 
-					'f_sugerido'   => $dia[2].'-'.$dia[1].'-'.$dia[0],
-					'hora_sug'     => $hora.':'.$min,
 					'id_equipo'    => $equipId,
 					'estado'       => 'S',	// graba estado Solicitado, cambia a 'C' en Ord Serv
 					'usrId'        => $usrId,
-					'solicitante'  => $solicita,
+					'solicitante'  => $usrName,
 					'causa'        => $falla,
 					'foto'         => 'assets/files/orders/sinImagen.jpg',
 					'id_empresa'   => $empId
@@ -182,7 +176,19 @@ class Sservicios extends CI_Model
 		}	
 	/* 	./ FUNCIONES ORIGINALES DE ASSET	*/
 
-	
+			// guarda adjunto en Edicion y en SS nueva
+			function setAdjunto($adjunto,$ultimoId){	
+				$this->db->where('id_solicitud', $ultimoId);
+				$query = $this->db->update("solicitud_reparacion",$adjunto);
+				return $query;
+			}
+
+			public function eliminar($id)
+			{
+				$this->db->where('id_solicitud', $id);
+				return $this->db->delete('solicitud_reparacion');
+			}
+
 /* INTEGRACION CON BPM */
 	// trae tareas STD para llenar select por empresa logueada
 		function getTareasStandar(){
