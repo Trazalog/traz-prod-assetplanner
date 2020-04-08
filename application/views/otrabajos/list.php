0b50b03f005bc77f78d9d020863bde92bbf3d4b9
Villegas Mathias <mvillegas@trazalog.com>
2020-04-08 09:51:27 -0300

    Quita boton agregar
    en pedido de materiales cuando entro por OT

--- application/views/otrabajos/list.php
+++ application/views/otrabajos/list.php
@@ -1383,9 +1383,12 @@ function guardarpedido(){
     console.log("ID de orden de trabajo para mostrar pedido es: "+idorde);  
    
     WaitingOpen();
-    $('#box-body').empty();
-    $("#box-body").load("<?php echo base_url(); ?>index.php/<?php echo ALM ?>/new/Pedido_Material/getPedidos/"+idorde);
-    WaitingClose(); 
+    
+    $('#content').empty();
+    $('#content').load("<?php echo base_url(); ?>index.php/<?php echo ALM ?>/new/Pedido_Material/getPedidos/"+idorde,{
+      $('.box-header').hide();
+    });
+    WaitingClose();
   };
 
 // AGREGAR NOTA DE PEDIDO
