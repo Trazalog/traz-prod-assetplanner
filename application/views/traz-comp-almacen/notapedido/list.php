1a03b756da18b5316a763700ab030948339f73c3
Hugo Gallardo <hugoeduardogallardo@gmail.com>
2021-01-04 16:07:15 -0300

    Comentado permiso ADD JUDAICO
    - btn ADD en views/traz-comp-almacen/notapedido/list.php

--- application/views/traz-comp-almacen/notapedido/list.php
+++ application/views/traz-comp-almacen/notapedido/list.php
@@ -4,7 +4,7 @@
             <h3 class="box-title">Pedido Materiales</h3>
             <?php
              
-             if (strpos($permission,'Add') !== false) {
+             //if (strpos($permission,'Add') !== false) {
                 if(!viewOT)
                 {
                 echo '<button class="btn btn-block btn-primary" style="width: 100px; margin-top: 10px;"
@@ -14,7 +14,7 @@
                 onclick="AbrirModal()">Agregar</button>';
                 }
                
-            }
+            //}
              
             
             if(isset($descripcionOT))
